6d37cc8906fed3d1c03ac9999875387bb7721386
anichan <qufekf97@gmail.com>
2023-10-05 16:02:45 +0900

    route 폴더 삭제 및 router 리팩토링

--- darknight/src/App.js
+++ darknight/src/App.js
@@ -1,13 +1,31 @@
+import CreateRoadmap from "./page/createRoadmap/createRoadmapIndex";
 import Header from "./page/layout/header";
+import MainPageIndex from "./page/mainPage/MainPageIndex";
+import MyPageIndex from "./page/myPage/MyPageIndex";
+import PostDetailIndex from "./page/postDetailPage/postDetailIndex";
+import PostPageIndex from "./page/postPage/postPageIndex";
+import RoadMapDetailIndex from "./page/roadMapDetail/roadMapDetailIndex";
+import RoadMapPageIndex from "./page/roadMapPage/roadMapPageIndex";
+import WritingPage from "./page/writingPage/writingPage";
 import Routing from "./route/routings";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Header>
-        <Routing />
+        <Routes>
+          <Route index element={<MainPageIndex/>}/>
+          <Route path="/mypage" element={<MyPageIndex/>}/>
+          <Route path="/post" element={<PostPageIndex/>}/>
+          <Route path="/createRoadmap" element={<CreateRoadmap/>}/>
+          <Route path="/roadMap" element={<RoadMapPageIndex/>}/>
+          <Route path="/postDetail" element={<PostDetailIndex/>}/>
+          <Route path="/roadMapDetail" element={<RoadMapDetailIndex/>}/>
+          <Route path="/writingPage" element={<WritingPage/>}/>
+          {/* <Route path="/login" element={<Login/>}/> */}
+        </Routes>
       </Header>
     </BrowserRouter>
   );
