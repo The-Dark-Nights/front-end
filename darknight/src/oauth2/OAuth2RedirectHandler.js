8ea45887249a2f0ee4e2414885800f56b2db4620
anichan <qufekf97@gmail.com>
2023-10-06 13:25:42 +0900

    로그인 JWT토큰, 리다이렉트 초기 개발

---
+++ darknight/src/oauth2/OAuth2RedirectHandler.js
@@ -0,0 +1,34 @@
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+
+function OAuth2RedirectHandler() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const getUrlParameter = (name) => {
+        name = name.replace(/[\\[]/, '\\[').replace(/[\]]/, '\\]');
+        const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+
+        const results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    };
+    useEffect(()  =>{
+        const token = getUrlParameter('token');
+        const error = getUrlParameter('error');
+    
+        // 로그인 됐을 때
+        if (token) {
+            localStorage.setItem(ACCESS_TOKEN, token);
+            localStorage.setItem(REFRESH_TOKEN, null); 
+            navigate(-1);     
+        }
+        // 로그인 안됐을 때
+        else{
+            navigate('/');
+        }
+            
+        },
+        []);
+}
