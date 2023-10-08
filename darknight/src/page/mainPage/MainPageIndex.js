d3cb00cc28ccdd36f50d3722e2abeff58dd81906
jeongminlee <masdf@naver.com>
2023-10-08 15:47:40 +0900

    rollingslidetest

--- darknight/src/page/mainPage/MainPageIndex.js
+++ darknight/src/page/mainPage/MainPageIndex.js
@@ -1,9 +1,10 @@
-
 import MainRadMap from "./component/mainroadmap";
 import MainPost from "./component/mainpost";
+import Homepage from "./component/testSlide";
 function MainPageIndex() {
   return (
     <>
+      <Homepage />
       <MainRadMap />
       <MainPost />
     </>
