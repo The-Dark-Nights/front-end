7d6054fc866fe1fc41085838989ece6be62a7406
jeongmin lee <maxsdf@naver.com>
2023-10-10 07:54:33 +0900

    fg

--- darknight/src/page/mainPage/component/mainroadmap.js
+++ darknight/src/page/mainPage/component/mainroadmap.js
@@ -1,7 +1,38 @@
 import styled from "styled-components";
 import RoadMapCard from '../../commondcomponent/roadMapCard'
 import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 function MainRadMap(){
+  const TOTAL_SLIDES = 30;
+  const imgLength = 1000;
+  const [curruntIdx, setCurrentIdx] = useState(0);
+    const [count, setCount] = useState(0);
+    const slideRef = useRef(null);
+
+    const nextSlide = () => {
+        if (curruntIdx >= TOTAL_SLIDES) {
+            setCurrentIdx(0);
+        } else {
+            setCurrentIdx((prev) => prev + 1);
+        }
+    };
+
+    const prevSlide = () => {
+        if (curruntIdx === 0) {
+            setCurrentIdx(TOTAL_SLIDES);
+        } else {
+            setCurrentIdx((prev) => prev - 1);
+        }
+    };
+    useEffect(() => {
+      const timer = setInterval(() => {
+          setCount((prev) => (prev === TOTAL_SLIDES ? 0 : prev + 1));
+      }, 300);
+
+      return () => {
+          clearInterval(timer);
+      };
+  }, [count]);
     return(
         <div className="roadMapListBox">
         {/* <!-- 로드맵네비 --> */}
@@ -18,15 +49,22 @@ function MainRadMap(){
           </Link>
         </RoadMapListNav>
         {/* <!-- 로드맵슬라이드 --> */}
-        <RoadMapSlide>
-          <RoadMapList>
+        <RoadMapSlide >
+          <RoadMapList ref={slideRef} count={count}>
             {/* <!-- 로드맵카드 --> */}
+            <RoadMapCard />
+            <RoadMapCard />
+            <RoadMapCard />
+            <RoadMapCard />
+            <RoadMapCard />
+            <RoadMapCard />
             <RoadMapCard />
             <RoadMapCard />
             <RoadMapCard />
             <RoadMapCard />
           </RoadMapList>
         </RoadMapSlide>
+          
       </div>
     )
 }
@@ -55,6 +93,8 @@ margin-top: 40px;
 overflow: hidden;
 `
 let RoadMapList=styled.ul`
+transition: ${(props) => (!props.count ? '' : 'all 1s ease-in-out')};
+    transform: ${(props) => 'translateX(-' + props.count * 50 + 'px)'};
 width: 2000px;
 
 overflow: hidden;
