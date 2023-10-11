4c280b77799046b17009dc874c9868639f3776df
jeongmin lee <maxsdf@naver.com>
2023-10-11 16:41:43 +0900

    'test삭제'

--- darknight/src/page/mainPage/MainPageIndex.js
+++ darknight/src/page/mainPage/MainPageIndex.js
@@ -1,7 +1,6 @@
 import MainRadMap from "./component/mainroadmap";
 import MainPost from "./component/mainpost";
-import Homepage from "./component/testSlide";
-import TestSlide2 from "./component/testSlide2";
+
 function MainPageIndex() {
   return (
     <>
